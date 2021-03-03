a6adf6d6b0d3d2c432ccb885f121cd6afc142485
Junior marques <jrmarqueshd@gmail.com>
2021-03-03 19:39:31 -0300

    turn to responsible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
 import "../styles/global.css";
 
-import { ChallengeProvider } from "../context/context-challenge";
-
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
